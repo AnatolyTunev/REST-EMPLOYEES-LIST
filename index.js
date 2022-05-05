1d43b52d9d05d1b6b559fc5ab5c8e7f4f5ca9dbc
AnatolyTunev <93310817+AnatolyTunev@users.noreply.github.com>
2022-05-05 20:14:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ const emplistRouter = require("./emplistr");
 
 app.get('/', (req, res) => {
   res.json({'message': 'ok'});
-})
+});
 
-app.use('/api/employees', emplistRouter);
+app.get("/api/employees", function(req, res){
+       
+    const content1 = '[{"pernr":"100","nachn":"Иван","vorna":"Федотов","birthdate":"13.03.1990"}';
+    const content2 = ',{"pernr":"200","nachn":"Игорь","vorna":"Семенов","birthdate":"01.01.1971"}';
+    const content3 = ',{"pernr":"300","nachn":"Федор","vorna":"Федотов","birthdate":"13.03.1990"}';
+    const content4 = ',{"pernr":"400","nachn":"Семен","vorna":"Семенов","birthdate":"01.01.1971"}';
+    const content5 = ',{"pernr":"500","nachn":"Павел","vorna":"Павлов","birthdate":"21.01.1972"}';
+    const content6 = ',{"pernr":"600","nachn":"Светлана","vorna":"Светличная","birthdate":"08.03.1989"}]';
+    const content = content1 + content2 + content3 + content4 + content5 + content6;
+    const employees = JSON.parse(content);
+    res.send(employees);
+});
+
+//app.use('/api/employees', emplistRouter);
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
